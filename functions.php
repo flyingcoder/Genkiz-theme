21bdfec59a16a0e7101584801eb587db16d530d5
AlvinP. <aeptechnologies@gmail.com>
2024-10-14 23:06:32 +0800

    Genkiz WordPress Theme initial package

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+
+
+function genkiz_theme_setup()
+{
+    add_theme_support('wp-block-styles');
+}
+add_action('after_setup_theme', 'genkiz_theme_setup');
+
+function my_custom_blocks()
+{
+    register_block_type( get_template_directory() . '/build/hero-block/block.json' );
+    register_block_type( get_template_directory() . '/build/testimonial/block.json' );
+}
+add_action('init', 'my_custom_blocks');
